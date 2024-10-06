53af456f8981ac45e3a6df092639b472cfd662da
Peter Mullen <peterjohnmullen@gmail.com>
2024-10-06 16:37:22 +0800

    Update to client intake form

--- pages/new-client.vue
+++ pages/new-client.vue
@@ -19,63 +19,52 @@
           <main class="main-content">
 
             <form name="New Client" method="POST" netlify>
-              <p><label>First Name:</label>
-              <input type="text" name="firstName"></p>
-              <p><label>Last Name:</label>
-              <input type="text" name="lastName"></p>
-              <p><label>Email Address:</label>
+              <p><label>First Name</label>
+              <input type="text" name="firstName" required></p>
+              <p><label>Last Name</label>
+              <input type="text" name="lastName" required></p>
+              <p><label>Email Address</label>
               <input type="text" name="email"></p>
-              <p><label>Phone Number:</label>
+              <p><label>Phone Number</label>
               <input type="text" name="phoneNumber"></p>
-              <p><label>Address:</label>
+              <p><label>Address</label>
               <textarea name="address" rows="2"></textarea></p>
-              <p><label>Date of Birth:</label>
+              <p><label>Date of Birth</label>
               <input type="date" name="dateOfBirth"></p>
 
               <h3>Client History</h3>
-              <p><label>Medical History:</label>
+              <p><label>Relevant Medical History <br /> <span>(i.e Injuries, surgeries, medical plans, general aches & pains, pregnant, post-natal)</span></label>
               <textarea name="medicalHistory"></textarea></p>
-              <p><label>Medication:</label>
+              <p><label>Relevant Medication</label>
               <textarea name="medication"></textarea></p>
-              <label>Are you pregnant?</label> <br />
-              <p><input type="radio" name="pregnancy" value="yes">
-              <label>Yes &nbsp;&nbsp;</label>
-              <input type="radio" name="pregnancy" value="no">
-              <label>No</label></p>
-              <p><label>If yes, how many weeks?</label>
-              <input type="text" name="pregnancyWeeks"></p>
-              <label>Are you post-natal?</label> <br />
-              <p><input type="radio" name="postNatal" value="yes">
-              <label>Yes &nbsp;&nbsp;</label>
-              <input type="radio" name="postNatal" value="no">
-              <label>No</label></p>
-              <p><label>If yes, how many weeks?</label>
-              <input type="text" name="postNatalWeeks"></p>
 
               <h3>Client Daily Movement</h3>
-              <p><label>Current level of physical activity:</label>
+              <p><label>Current Exercise</label>
               <textarea name="physicalActivity"></textarea></p>
-              <p><label>Occupation:</label>
+              <p><label>Occupation<br /><span>(i.e stay at home mum, part time or part-time/full time work)</span></label>
               <input type="text" name="occupation"></p>
-              <p><label>Physical movement during the day:</label>
+              <p><label>Physical movement required for occupation<br /><span>(i.e running around after kids all day, waitressing tables, computer work, combination)</span></label>
               <textarea name="dailyMovement"></textarea></p>
-              <p><label>How much sleep do you get at night?</label>
-              <input type="text" name="sleep"></p>
 
               <h3>Client Goals</h3>
-              <p><label>Goals for the 6-week challenge:</label>
+              <p><label>Goals for the 6-week challenge</label>
               <textarea name="goals"></textarea></p>
 
+              <h3>Home Equipment</h3>
+              <p><label>What exercise equipment do you have at home<br /><span>(i.e weights, TheraBand, massage ball, yoga ball, treadmill, bike)</span></label>
+              <textarea name="goals"></textarea></p>
+
+
               <h3>Emergency Contact</h3>
-              <p><label>First Name:</label>
+              <p><label>First Name</label>
               <input type="text" name="emergencyFirstName"></p>
-              <p><label>Last Name:</label>
+              <p><label>Last Name</label>
               <input type="text" name="emergencyLastName"></p>
-              <p><label>Contact Number:</label>
+              <p><label>Contact Number</label>
               <input type="text" name="emergencyPhoneNumber"></p>
 
               <p><input type="checkbox" name="termsAndConditions">
-              <label> &nbsp; I understand that by submitting this form and signing up to Move-To Pilates Classes, you agree to be bound by the <a href="https://example.com/terms-and-conditions">Terms and Conditions</a> and <a href="https://example.com/liability-waiver">Liability Waiver</a>. These Terms and Conditions may change at Move-To Pilates discretion, and you may not be notified of this change.</label></p>
+              <label> &nbsp; I understand that by submitting this form and signing up to Move-To Pilates’ sessions, you agree to be bound by the <a href="https://example.com/terms-and-conditions">Terms and Conditions</a> and <a href="https://example.com/liability-waiver">Liability Waiver</a>. These Terms and Conditions may change at Move-To Pilates’ discretion, and you may not be notified of this change.</label></p>
 
               <p><button type="submit" class="button">Submit</button></p>
           </form>
