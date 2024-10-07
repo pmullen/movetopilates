7366b2095325b9eb0fc891fc5ec5258ccba56f39
Peter Mullen <peterjohnmullen@gmail.com>
2024-10-07 08:28:51 +0800

    Link terms from footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="site-footer section-footer">
     <div class="container container-m">
-      <p>&copy; Move to Pilates - Terms &amp; Conditions</p>
+      <p>&copy; Move to Pilates - <NuxtLink to="/terms-conditions">Terms &amp; Conditions</NuxtLink></p>
     </div><!-- #end container -->
   </footer><!-- #end site-footer -->
 </template>
