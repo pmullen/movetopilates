9f3f63cfcfb26aa55ebad90d5aaf35c9c216beb4
Peter Mullen <peter@pmullen.com>
2025-07-24 13:04:58 +0800

    New intake form on HubSpot

--- pages/new-client/index.vue
+++ pages/new-client/index.vue
@@ -18,51 +18,8 @@
 
           <main class="main-content">
 
-            <form name="New Client" method="POST" netlify>
-              <p><label>First Name</label>
-              <input type="text" name="firstName"> </p>
-              <p><label>Last Name</label>
-              <input type="text" name="lastName"></p>
-              <p><label>Email Address</label>
-              <input type="text" name="email"></p>
-              <p><label>Phone Number</label>
-              <input type="text" name="phoneNumber"></p>
-              <p><label>Address</label>
-              <textarea name="address" rows="2"></textarea></p>
-              <p><label>Date of Birth</label>
-              <input type="date" name="dateOfBirth"></p>
+            <div id="hubspot-form"></div>
 
-              <h3>Client History</h3>
-              <p><label>Relevant Medical History <br /> <span>(i.e Injuries, surgeries, medical plans, general aches & pains, pregnant, post-natal)</span></label>
-              <textarea name="medicalHistory"></textarea></p>
-              <p><label>Relevant Medication</label>
-              <textarea name="medication"></textarea></p>
-
-              <h3>Client Daily Movement</h3>
-              <p><label>Current Exercise</label>
-              <textarea name="physicalActivity"></textarea></p>
-              <p><label>Occupation<br /><span>(i.e stay at home mum, part time or part-time/full time work)</span></label>
-              <input type="text" name="occupation"></p>
-              <p><label>Physical movement required for occupation<br /><span>(i.e running around after kids all day, waitressing tables, computer work, combination)</span></label>
-              <textarea name="dailyMovement"></textarea></p>
-
-              <h3>Client Goals</h3>
-              <p><label>Exercise Goals This Term<br /><span>(e.g. attend every class, build core strength, reduce back pain)</span></label>
-              <textarea name="goals"></textarea></p>
-
-              <h3>Emergency Contact</h3>
-              <p><label>First Name</label>
-              <input type="text" name="emergencyFirstName"></p>
-              <p><label>Last Name</label>
-              <input type="text" name="emergencyLastName"></p>
-              <p><label>Contact Number</label>
-              <input type="text" name="emergencyPhoneNumber"></p>
-
-              <p><input type="checkbox" name="termsAndConditions" required>
-              <label> &nbsp; I understand that by submitting this form and signing up to Move-To Pilates’ sessions, you agree to be bound by the <a href="/terms-conditions" target="_blank">Terms and Conditions</a> and <a href="/liability" target="_blank">Liability Waiver</a>. These Terms and Conditions may change at Move-To Pilates’ discretion, and you may not be notified of this change.</label></p>
-
-              <p><button type="submit" class="button">Submit</button></p>
-          </form>
         </main><!-- #end main-content -->
 
     </div><!-- #end container -->
@@ -111,6 +68,27 @@
         }
       ],
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
+    script: [
+      {
+        charset: "utf-8",
+        type: "text/javascript",
+        src: "//js-ap1.hsforms.net/forms/embed/v2.js"
+      },
+      {
+        type: "text/javascript",
+        innerHTML: `
+          hbspt.forms.create({
+            portalId: "442003764",
+            formId: "b0f2e865-a2ae-4544-a689-9115772a4cdb",
+            region: "ap1",
+            target: "#hubspot-form" // The target div where the form will be rendered
+          });
+        `
+      }
+    ],
+    __dangerouslyDisableSanitizersByTagID: {
+      'id-form-script': ['innerHTML']
     }
   }
 </script>
