48a3f327d1dd0aac03a50104f9dc458cd0e23467
Peter Mullen <peter@pmullen.com>
2025-09-23 21:27:16 +0800

    Changes to pricing page and nav

--- components/Header.vue
+++ components/Header.vue
@@ -7,58 +7,100 @@
             Move to Pilates
           </NuxtLink>
 
-          <NuxtLink to="pricing" class="button button-nav button-demo">Pricing &nbsp; &raquo;</NuxtLink>
+          <div class="nav-group">
+            <nav class="main-nav">
+              <NuxtLink to="/pricing" class="nav-link">Pricing</NuxtLink>
+              <NuxtLink to="/timetable" class="nav-link">Timetable</NuxtLink>
+            </nav>
+            <a href="https://momence.com/m/534120" class="button button-nav button-demo" target="_blank" rel="noopener">
+              Book Intro Offer &nbsp; &raquo;
+            </a>
+          </div>
         </div><!-- #end grid -->
       </div><!-- #end container -->
     </header><!-- #end site-header -->
-
   </div>
 </template>
 
 <style lang="scss" scoped>
+.site-header {
+  background: white;
+  height: 100px;
+}
+
+@media only screen and (max-width: 960px) {
   .site-header {
-    background: white;
-    height: 100px;
+    height: 80px;
   }
+}
 
-  @media only screen and (max-width: 960px) {
-    .site-header {
-      height: 80px;
-    }
+@media only screen and (max-width: 500px) {
+  .site-header {
+    height: 70px;
   }
+}
 
-  @media only screen and (max-width: 500px) {
-    .site-header {
-      height: 70px;
-    }
-  }
+.site-logo {
+  background: url(../assets/img/logo/site-logo.png) no-repeat;
+  background-size: cover;
+  display: block;
+  height: 50px;
+  margin: auto 0 auto 0;
+  width: 131px
+}
 
+@media only screen and (max-width: 600px) {
   .site-logo {
-    background: url(../assets/img/logo/site-logo.png) no-repeat;
-    background-size: cover;
-    display: block;
-    height: 50px;
-    margin: auto 0 auto 0;
-    width: 131px
+    height: 40px;
+    width: 105px;
   }
+}
+
+.flex {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.nav-group {
+  display: flex;
+  align-items: center;
+}
+
+.main-nav {
+  display: flex;
+  align-items: center;
+  margin-right: 20px; 
+
+  .nav-link {
+    color: #111111;
+    text-decoration: none;
+    font-weight: 500;
+    margin-right: 1.5rem;
+    font-size: 1rem;
+    transition: color 0.2s;
+
+    &:last-child {
+      margin-right: 0;
+    }
+
+    &:hover {
+      color: var(--primary-color, #2d3a5a);
+      text-decoration: none;
+    }
+  }
+}
+
+.button-nav {
+  margin: 0;
 
   @media only screen and (max-width: 600px) {
-    .site-logo {
-      height: 40px;
-      width: 105px;
-    }
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    border-radius: 0;
+    z-index: 1;
   }
-
-  .button-nav {
-    margin: auto 0 auto auto;
-
-    @media only screen and (max-width: 600px) {
-      position: fixed;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      border-radius: 0;
-      z-index: 1;
-    }
-  }
+}
 </style>
--- pages/pricing.vue
+++ pages/pricing.vue
@@ -27,35 +27,7 @@
             </div><!-- #end item-featured -->
           </div>
 
-          <h3>Memberships</h3>
-          <p>Memberships are designed to keep you consistent and on track with your exercise every 4 weeks.</p>
-          <p>Each 4-week block includes 8 or 12 classes, giving you the best opportunity to stay motivated and work towards your goals — whether that’s weight loss, relieving pain, strengthening your core, building a regular exercise routine, prioritising self-care through movement, or focusing on your own personal goals.</p>
-
-          <p>Memberships can be cancelled at any time with 2 weeks’ notice prior to the next auto-renew date.</p>
-
-          <div class="grid grid-3 list-benefits">
-            <div class="item-featured">
-              <h4>12 Class Pack (2 per week)</h4>
-              <ul>
-                <li>$20 per class</li>
-                <li>2 x class per week</li>
-                <li>$160 auto renews every 28 days</li>
-              </ul>
-              <p><a href="https://momence.com/m/534106" class="button">Sign Up &nbsp; &raquo;</a></p>
-            </div><!-- #end item-featured -->
-
-            <div class="item-featured">
-              <h4>12 Class Pack (3 per week)</h4>
-              <ul>
-                <li>$20 per class</li>
-                <li>3 x class per week</li>
-                <li>$240 auto renews every 28 days</li>
-              </ul>
-              <p><a href="https://momence.com/m/534108" class="button">Sign Up &nbsp; &raquo;</a></p>
-            </div><!-- #end item-featured -->
-          </div>
-
-          <h3>10 Pack</h3>
+          <h3>Class Packs</h3>
           <p>Class packs are designed to give you more flexibility — perfect if you know you’ll be away for an extended period. Each pack is valid for up to 6 months, giving you plenty of time to use your sessions at your own pace.</p>       
 
           <div class="grid grid-3 list-benefits">
@@ -67,8 +39,56 @@
               </ul>
               <p><a href="https://momence.com/Move-To-Pilates/membership/-10-Class-Package-/532767" class="button">Sign Up &nbsp; &raquo;</a></p>
             </div><!-- #end item-featured -->
+
+            <div class="item-featured">
+              <h4>20 Class Pack</h4>
+              <ul>
+                <li>$22 per class</li>
+                <li>Valid for 12 months</li>
+              </ul>
+              <p><a href="https://momence.com/m/534110" class="button">Sign Up &nbsp; &raquo;</a></p>
+            </div><!-- #end item-featured -->
           </div>
 
+
+          <h3>Memberships</h3>
+          <p>Memberships are designed to keep you consistent and on track with your exercise every 4 weeks.</p>
+          <p>Each 4-week block includes 8 or 12 classes, giving you the best opportunity to stay motivated and work towards your goals — whether that’s weight loss, relieving pain, strengthening your core, building a regular exercise routine, prioritising self-care through movement, or focusing on your own personal goals.</p>
+
+          <p>Memberships can be cancelled at any time with 2 weeks’ notice prior to the next auto-renew date.</p>
+
+          <div class="grid grid-3 list-benefits">
+            <div class="item-featured">
+              <h4>8 Class Membership (2 per week)</h4>
+              <ul>
+                <li>$20 per class</li>
+                <li>2 x class per week</li>
+                <li>$160 auto renews every 28 days</li>
+              </ul>
+              <p><a href="https://momence.com/m/534106" class="button">Sign Up &nbsp; &raquo;</a></p>
+            </div><!-- #end item-featured -->
+
+            <div class="item-featured">
+              <h4>12 Class Membership (3 per week)</h4>
+              <ul>
+                <li>$20 per class</li>
+                <li>3 x class per week</li>
+                <li>$240 auto renews every 28 days</li>
+              </ul>
+              <p><a href="https://momence.com/m/534108" class="button">Sign Up &nbsp; &raquo;</a></p>
+            </div><!-- #end item-featured -->
+
+            <div class="item-featured">
+              <h4>4 Class Membership (1 per week)</h4>
+              <ul>
+                <li>$20 per class</li>
+                <li>1 x class per week</li>
+                <li>$80 auto renews every 28 days</li>
+              </ul>
+              <p><a href="https://momence.com/m/534109" class="button">Sign Up &nbsp; &raquo;</a></p>
+            </div><!-- #end item-featured -->
+
+          </div><!-- #end grid -->
 
         </div><!-- #end container -->
 
